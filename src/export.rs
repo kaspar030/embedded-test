56443a112c7efaa21b73e6a0ecaefd5f3af3b7b4
Kaspar Schleiser <kaspar@schleiser.de>
2024-11-07 15:15:14 +0100

    feat: add RIOT-rs support

--- src/export.rs
+++ src/export.rs
@@ -15,9 +15,13 @@ pub fn ensure_linker_file_was_added_to_rustflags() -> ! {
 }
 
 // Reexport the embassy stuff
-#[cfg(feature = "embassy")]
+#[cfg(all(feature = "embassy", not(feature = "riot-rs")))]
 pub use embassy_executor::task;
-#[cfg(all(feature = "embassy", not(feature = "external-executor")))]
+#[cfg(all(
+    feature = "embassy",
+    not(feature = "external-executor"),
+    not(feature = "riot-rs")
+))]
 pub use embassy_executor::Executor; // Please activate the `executor-thread` or `executor-interrupt` feature on the embassy-executor crate (v0.5.x/v0.6.x)!
 
 const VERSION: u32 = 1; //Format version of our protocol between probe-rs and target running embedded-test
